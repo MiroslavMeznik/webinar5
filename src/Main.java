079ee7cdd3993e00dc038d88a09b33b142bbbc6f
Miroslav Meznik <mez.mir@seznam.cz>
2025-07-07 15:44:09 +0200

    test branch

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("Hello and welcome!"); //change f to ln
         System.out.println("chacha");
         System.out.println("Manual commit");
-        System.out.println("new branch");
+        System.out.println("new branch test");
 
 //        for (int i = 1; i <= 5; i++) {
 //            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
